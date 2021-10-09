935650cf2dfdd6f514df921f363f824f74400227
Bintang0805 <ikhsanbintang3292@gmail.com>
2021-10-10 01:27:22 +0700

    Add navbar and font awesome

---
+++ resources/views/components/layouts/navbar.blade.php
@@ -0,0 +1,5 @@
+<div class="fixed top-0 left-0">
+    <div class="navbar px-12">
+        <h1>test</h1>
+    </div>
+</div>
--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -3,6 +3,42 @@
         {{ $title }}
     </x-slot>
 
+    <div id="navbar" class="fixed top-0 left-0 px-5 w-full sm:px-12 py-5">
+        <div class="navbar">
+            <div class="grid grid-cols-12">
+                <div class="col-span-8">
+                    <div class="nav_title flex items-center text-white text-xl font-medium text-light">
+                        My Portofolio <i class="fas fa-star text-base ml-3"></i>
+                    </div>
+                </div>
+                <div class="col-span-3">
+                    <ul class="flex gap-3 text-white items-center justify-between font-medium">
+                        <li>
+                            <a href="" class="text-light border-b-2 border-light">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Projects
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
     {{-- Page One --}}
 
     <div id="page-one" class="w-full px-5 sm:px-12 h-screen bg-darken-blue">
